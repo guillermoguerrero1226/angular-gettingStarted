77e366e70338bf244000b5144252398111a3047e
Guillermo Guerrero <william.guerrero@nserio.com>
2018-03-26 17:18:22 -0500

    no message

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 
 @Component({
@@ -14,7 +15,7 @@ export class ProductListComponent implements OnInit{
     imageWidth:number=50;
     imageMargin:number=2;
     showImage:boolean=false;
-
+    errorMessage: string;
     _listFilter:string;
     get listFilter():string {
         return this._listFilter;
@@ -24,33 +25,9 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts= this._listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[]=[
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.5,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-        
-        
-    ];
+    products: IProduct[]=[];
 
-    constructor(){
-        this.filteredProducts=this.products;
+    constructor(private _productService: ProductService){
         this.listFilter='cart';
     }
 
@@ -59,7 +36,14 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log("Method init");
+        this._productService.getProducts()
+                .subscribe(products => {
+                    this.products = products,
+                    this.filteredProducts=this.products;
+                },
+                error => this.errorMessage =<any>error);
+
+        
     }
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
